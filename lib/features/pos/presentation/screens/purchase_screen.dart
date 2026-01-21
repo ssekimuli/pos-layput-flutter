71b6c1934ee3dcc21e0ab7e6258810e69c25c51b
ssekimuli <83583896+ssekimuli@users.noreply.github.com>
2026-01-21 16:09:54 +0300

    fix

--- lib/features/pos/presentation/screens/purchase_screen.dart
+++ lib/features/pos/presentation/screens/purchase_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pos_desktop_ui/models/product.dart';
+import 'package:pos_desktop_ui/core/models/product.dart';
 
 class PurchaseScreen extends StatelessWidget {
   final Product product;
