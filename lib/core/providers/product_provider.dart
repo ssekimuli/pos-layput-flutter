d9faa0a1996ff0cb0815de05dfdda58824fabafb
ssekimuliandrew321 <ssekimuliandrew321@gmail.com>
2026-01-21 12:40:15 +0000

    add statemangement and fix foldeer structure

---
+++ lib/core/providers/product_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pos_desktop_ui/core/models/product.dart';
+
+class ProductNotifier extends StateNotifier<List<Product>> {
+  ProductNotifier() : super([]);
+
+  void setProducts(List<Product> products) {
+    state = products;
+  }
+}
+
+final productProvider = StateNotifierProvider<ProductNotifier, List<Product>>((ref) {
+  final productNotifier = ProductNotifier();
+  productNotifier.setProducts(List.generate(
+    20,
+    (i) => Product(
+      name: i % 2 == 0 ? 'Coffee Mug - 350ml' : 'Espresso Cup',
+      price: i % 2 == 0 ? 14.99 : 9.50,
+      color: i % 2 == 0
+          ? const Color.fromARGB(255, 199, 225, 233)
+          : const Color.fromARGB(255, 230, 219, 203),
+    ),
+  ));
+  return productNotifier;
+});
