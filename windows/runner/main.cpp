48a32df58862d8a6a97453c8f8e900dc8bb1391e
ssekimuli <83583896+ssekimuli@users.noreply.github.com>
2026-01-21 16:30:01 +0300

    fix not show

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,37 +5,33 @@
 #include "flutter_window.h"
 #include "utils.h"
 
-// 1. Correct Include
-#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
-
-// 2. Global configuration - this is the key!
-auto bdw = bitsdojo_window_configure(BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP);
-
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  
+  // Attach to console when present (e.g., 'flutter run') or create a
+  // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments =
+      GetCommandLineArguments();
+
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
-  // 3. Use the STANDARD FlutterWindow (bitsdojo handles the rest behind the scenes)
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
   if (!window.Create(L"pos_desktop_ui", origin, size)) {
     return EXIT_FAILURE;
   }
-  
   window.SetQuitOnClose(true);
 
-  // 4. Standard Windows Message Loop
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
